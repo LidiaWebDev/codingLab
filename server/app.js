58ca2ff1a72d331609e22b45aa319931229440e6
LidiaWebDev <davidsonlidia@gmail.com>
2020-07-18 01:07:29 -0700

    removedhomepage

--- server/app.js
+++ server/app.js
@@ -76,12 +76,11 @@ app.post('/api/post/getAllPost', (req, res) => {
 })
 //step3
 if(process.env.NODE_ENV ==='production') {
-  app.use(express.static(path.join(__dirname, "dist")))
-  // app.use(express.static("../codingBlog/dist"));
+ 
+  app.use(express.static("../codingBlog/dist"));
   app.get('*', (req, res) => {
-    // const app = path.join(__dirname, "../codingBlog/dist/codingBlog/");
-    // const index = path.join(__dirname, "../codingBlog/dist/codingBlog/", "index.html");
-    const app = path.join(__dirname, "/dist/codingBlog/index.html");
+    const app = path.join(__dirname, "../codingBlog/dist/codingBlog/");
+    const index = path.join(__dirname, "../codingBlog/dist/codingBlog/", "index.html");
     res.sendFile(app);   
     res.sendFile(index);   
   })
