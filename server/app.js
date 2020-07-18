6cec0775e2a2f11244549a33a8fd54f6f5ba547e
LidiaWebDev <davidsonlidia@gmail.com>
2020-07-17 23:33:28 -0700

    removedhomepage

--- server/app.js
+++ server/app.js
@@ -79,7 +79,9 @@ if(process.env.NODE_ENV ==='production') {
   console.log("this application is on Heroku")
   app.use(express.static("../codingBlog/dist"));
   app.get('*', (req, res) => {
+    const app = path.join(__dirname, "../codingBlog/dist/codingBlog/");
     const index = path.join(__dirname, "../codingBlog/dist/codingBlog/", "index.html");
+    res.sendFile(app);   
     res.sendFile(index);    
   
   });
