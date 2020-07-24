afd3946303d4df66a10a1f2095d199cf60602246
LidiaWebDev <davidsonlidia@gmail.com>
2020-07-24 15:54:35 -0700

    additional bundle

--- server/app.js
+++ server/app.js
@@ -92,11 +92,11 @@ if(process.env.NODE_ENV ==='production') {
   
   app.get('*', (req, res) => {
     if (allowed.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
-      res.sendFile(path.resolve(`../codingBlog/dist/${req.url}`));
+      res.sendFile(path.resolve(`codingLab/codingBlog/dist/${req.url}`));
     }
     else {
-    const app = path.join(__dirname, "../codingBlog/dist/");
-    const index = path.join(__dirname, "../codingBlog/dist/", "index.html");
+    const app = path.join(__dirname, "codingLab/codingBlog/dist/");
+    const index = path.join(__dirname, "codingLab/codingBlog/dist/", "index.html");
     res.sendFile(app);   
     res.sendFile(index);  
     } 
