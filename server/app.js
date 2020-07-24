7cba7e42fb455155b23af17f1af625e0471c40bc
LidiaWebDev <davidsonlidia@gmail.com>
2020-07-24 11:10:37 -0700

    additional bundle

--- server/app.js
+++ server/app.js
@@ -82,10 +82,10 @@ app.post('/api/post/getAllPost', (req, res) => {
 //step3
 if(process.env.NODE_ENV ==='production') {
  
-  app.use(express.static("../codingBlog/dist/codingBlog"));
+  app.use(express.static("dist/codingBlog"));
   app.get('*', (req, res) => {
-    const app = path.join(__dirname, "../codingBlog/dist/codingBlog/");
-    const index = path.join(__dirname, "../codingBlog/dist/codingBlog/", "index.html");
+    const app = path.join(__dirname, "dist/codingBlog/");
+    const index = path.join(__dirname, "dist/codingBlog/", "index.html");
     res.sendFile(app);   
     res.sendFile(index);   
   })
