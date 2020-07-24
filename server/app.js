2cbef8ac90f0795524da6b0497c5bfc7f43bed06
LidiaWebDev <davidsonlidia@gmail.com>
2020-07-24 14:19:30 -0700

    additional bundle

--- server/app.js
+++ server/app.js
@@ -82,7 +82,7 @@ app.post('/api/post/getAllPost', (req, res) => {
 //step3
 if(process.env.NODE_ENV ==='production') {
  
-  app.use(express.static(__dirname, "../codingBlog/dist/codingBlog/"));
+  app.use(express.static("dist/codingBlog"));
   const allowed = [
     '.js',
     '.css',
