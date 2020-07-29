d0bddbb0b3aec504dc2eed7cb6d6e99a9e9db3a0
LidiaWebDev <davidsonlidia@gmail.com>
2020-07-28 21:38:30 -0700

    service

--- server/app.js
+++ server/app.js
@@ -1,301 +1,99 @@
-var express = require('express')
-var path = require('path')
-var cors = require('cors')
-var morgan = require('morgan')
-var bodyParser = require('body-parser')
-var serveStatic = require('serve-static')
-const logger = require("morgan")
+var express = require("express");
+var path = require("path");
+var cors = require("cors");
+var morgan = require("morgan");
+var bodyParser = require("body-parser");
+var serveStatic = require("serve-static");
+const logger = require("morgan");
 
-const mongoose = require('mongoose')
-require('dotenv').config();
-var app = express()
+const mongoose = require("mongoose");
+require("dotenv").config();
+var app = express();
 
-//step1
-var PORT = process.env.PORT || 8080
-var Users = require('./routes/users')
+//step1 establishing connection for Heroku server
+var PORT = process.env.PORT || 8080;
+var Users = require("./routes/users");
+const MLAB_API = process.env.MLAB_API;
 
-app.use(cors({
-origin:['http://localhost:4200', 'http://127.0.0.1:4200'],
-credentials:true}));
+app.use(
+  cors({
+    origin: ["http://localhost:4200", "http://127.0.0.1:4200"],
+    credentials: true,
+  })
+);
 
-
-app.use(bodyParser.json())
-app.use(logger('combined'))
+app.use(bodyParser.json());
+app.use(logger("combined"));
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
-)
-
-// Create link to Angular build directory
-//app.use(serveStatic(path.join(__dirname + "./codingBlog/dist/codingBlog")))
+);
 
 //const MONGODB_URI = 'mongodb://localhost:27017/members'
-//step2
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/members"
+//step2 establishing connection with MongoDB via MbLab
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/members";
 mongoose
-.connect (MONGODB_URI,
+  .connect(
+    MONGODB_URI,
 
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected Externally"))
+  .catch((err) => console.log("Could not connect to the DB", err));
 
-//.connect('mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds011903.mlab.com:11903/${process.env.DB_NAME}',
-//.connect('mongodb://localhost:27017/members',
-//.connect ('mongodb://admin:koshatina8@127.0.0.1:27017/?authSource=heroku_rqlbmf6r',
-
-    // process.env.MONGODB_URI || 'mongodb://localhost:27017/members',
-    //'mongodb://port-folio1949:12!folio@ds011903.mlab.com:11903/heroku_rqlbmf6r',
-    { useNewUrlParser: true ,   
-    useCreateIndex: true, 
-    useUnifiedTopology: true,
-    useFindAndModify: false
-
-  })
-  .then(() => console.log('MongoDB Connected Externally'))
-  .catch(err => console.log('Could not connect to the DB', err))
-
-  //Data parsing
-  app.use(express.json())
-  app.use(express.urlencoded({extended: false}))
-  
- 
-
-
+//Data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //HTTP request logger
-app.use(morgan('tiny'))
-app.use('/users', Users)
+app.use(morgan("tiny"));
+app.use("/users", Users);
 
-const Post = require('./model/post')
+const Post = require("./model/post");
 //API end point for fetching the list of blog posts. Since for db Mongo is used, Mongoose client added to connect the db with the app.
-app.post('/api/post/getAllPost', (req, res) => {
-    mongoose.connect(url, { useMongoClient: true }, { useUnifiedTopology: true },function(err){
-      console.log(err - 'error here')
-        if(err) throw err;
-        console.log("connection established successfully")
-        Post.find({},[],{ sort: { _id: -1 } },(err, doc) => {
-            if(err) throw err;
-            return res.status(200).json({
-                status: 'success',
-                data: doc
-            })
-        })
-    });
-})
-//step3
-
-//app.use(express.static(path.join(__dirname, "../codingBlog/dist")))
-if(process.env.NODE_ENV ==='production') {
- 
+app.post("/api/post/getAllPost", (req, res) => {
+  mongoose.connect(
+    url,
+    { useMongoClient: true },
+    { useUnifiedTopology: true },
+    function (err) {
+      console.log(err - "error here");
+      if (err) throw err;
+      console.log("connection established successfully");
+      Post.find({}, [], { sort: { _id: -1 } }, (err, doc) => {
+        if (err) throw err;
+        return res.status(200).json({
+          status: "success",
+          data: doc,
+        });
+      });
+    }
+  );
+});
+//step3 getting paths for the static files for production
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../codingBlog/dist")));
   app.use(express.static(path.join(__dirname, "../codingBlog/dist/assets")));
-  // const allowed = [
-  //   '.js',
-  //   '.css',
-  //   '.png',
-  //   '.jpg'
-  // ];
-  
-  app.get('*', (req, res) => {
-    // if (allowed.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
-    //   res.sendFile(path.resolve(__dirname, `../codingBlog/dist/${req.url}`));
-    //   res.redirect("/codingBlog/") 
-    // }
-    // else {
+  app.get("*", (req, res) => {
     const index = path.join(__dirname, "../codingBlog/dist/", "index.html");
     res.sendFile(index);
-    res.redirect("/codingBlog/")  
-    
-  })
+    res.redirect("/codingBlog/");
+  });
 }
-
-
-
+//for the wrong path
 app.use((req, res, next) => {
   res.status(404).send({
-  status: 404,
-  error: "This page does not exist. You will have to check the correct routing"
-  })
- })
+    status: 404,
+    error:
+      "This page does not exist. You will have to check the correct routing",
+  });
+});
 
-
-app.listen(PORT, 
-  console.log(`Server is running successfully at ${PORT}!`))
-
-
-
-
-
-// require('./config/config')
-
-// //const createError = require('http-errors');
-// const express = require('express')
-// const cors = require('cors')
-// const bodyParser = require('body-parser')
-// const mongoose = require('mongoose')
-// const path = require ("path")
-// const cookieParser = require('cookie-parser')
-// const morgan = require('morgan')
-
-
-// const app = express()
-// //view engine setup
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(morgan('dev'));
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
-// app.use(cors({
-//     origin:['http://localhost:4200', 'http://127.0.0.1:4200'],
-//     credentials:true
-// }));
-
-
-
-
- 
-
-
-
-
-// //passport
-
-// const passport = require('passport');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo')(session);
-
-// app.use(session({
-//     name:'myname.sid',
-//     resave:false,
-//     saveUninitialized:false,
-//     secret:'secret',
-//     cookie:{
-//       maxAge:36000000,
-//       httpOnly:false,
-//       secure:false
-//     },
-//     store: new MongoStore({ mongooseConnection: mongoose.connection })
-//   }));
-//   require('./passport-config');
-//   app.use(passport.initialize());
-//   app.use(passport.session());
-  
-
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
-
-
-
-
-
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// const User = require('./model/user');
-// const Post = require('./model/post');
- 
-
-
-
-
-// app.use(bodyParser.urlencoded({ extended : false}))
-
-// app.post('/api/user/login', (req, res) => {
-//     mongoose.connect(url, { useNewUrlParser: true }, function(err){
-//         if(err) throw err;
-//         User.find({
-//             username : req.body.username, password : req.body.password
-//         }, function(err, user){
-//             if(err) throw err;
-//             if(user.length === 1){  
-//                 return res.status(200).json({
-//                     status: 'success',
-//                     data: user
-//                 })
-//             } else {
-//                 return res.status(200).json({
-//                     status: 'fail',
-//                     message: 'Login Failed'
-//                 })
-//             }
-             
-//         })
-//     });
-// })
-
-// //REST API endpoint to update post details based on the ID of the post
-
-// app.post('/api/post/updatePost', (req, res) => {
-//     mongoose.connect(url, { useMongoClient: true }, function(err){
-//         if(err) throw err;
-//         Post.update(
-//             {_id: req.body.id },
-//             { title : req.body.title, description: req.body.description },
-//             (err, doc) => {
-//             if(err) throw err;
-//             return res.status(200).json({
-//                 status: 'success',
-//                 data: doc
-//             })
-//         })
-//     });
-// })
-
-// //REST API end point for adding the blog post to the MongoDB database.
-
-// app.post('/api/post/createPost', (req, res) => {
-//     // insert the details to MongoDB
-//     //connect to the MongoDB database using the Mongoose client.
-//     mongoose.connect(url, { useMongoClient: true }, function(err){
-//         if(err) throw err;
-//         console.log('connection established ');
-//     //create a model object using the Post schema defined in the server/model/post.js file
-//     const post = new Post({
-//         title: req.body.title,
-//         description: req.body.description
-//     })
-//     //Call the save method on the Post object to save the entry to MongoDB.
-//     post.save((err, doc) => {
-//         if(err) throw err;
-//         return res.status(200).json({
-//             status: 'success',
-//             data: doc
-//         })
-//     })
-//   });
-// })
-
-// //REST API end point to delete the blog post to the MongoDB database.
-
-// app.post('/api/post/deletePost', (req, res) => {
-//     mongoose.connect(url, { useMongoClient: true }, function(err){
-//         if(err) throw err;
-//         Post.findByIdAndRemove(req.body.id,
-//             (err, doc) => {
-//             if(err) throw err;
-//             return res.status(200).json({
-//                 status: 'success',
-//                 data: doc
-//             })
-//         })
-//     });
-// })
-
-
-// const AuthRoute = require( './routes/users')
-// app.use('/users', AuthRoute)
-// app.listen(3000, () => console.log('blog server running on port 3000!'))
+app.listen(PORT, console.log(`Server is running successfully at ${PORT}!`));
